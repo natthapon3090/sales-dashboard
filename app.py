486597d86b56f3890d003a1719c76cfa72a1c52f
natthapon3090 <garfield11kp@gmail.com>
2026-02-20 23:03:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
+from dash import Dash, dcc, html, Input, Output
+import plotly.express as px
+import pandas as pd
 
+# โหลดไฟล์จากโฟลเดอร์เดียวกัน
+df = pd.read_csv("superstore.csv")
+df['Order Date'] = pd.to_datetime(df['Order Date'])
+
+app = Dash(__name__)
+app.title = "Sales Dashboard"
+
+app.layout = html.Div([
+    html.H1("📊 Global Sales Dashboard", style={'textAlign': 'center'}),
+
+    html.Div([
+        dcc.Dropdown(
+            id='region-dropdown',
+            options=[{'label': r, 'value': r} for r in df['Region'].unique()],
+            value=df['Region'].unique()[0],
+            placeholder="Select Region"
+        ),
