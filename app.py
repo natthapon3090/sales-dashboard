dc29700e5d205f63e979dc184457b056e9f2c3f6
natthapon3090 <garfield11kp@gmail.com>
2026-02-20 23:19:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,3 +12,11 @@ app = Dash(__name__)
 app.title = "NVIDIA Dashboard"
 
 app.layout = html.Div([
+    html.H1("📈 NVIDIA Stock Dashboard", style={"textAlign": "center"}),
+
+    html.Div([
+        dcc.DatePickerRange(
+            id="date-picker",
+            start_date=df["Date"].min(),
+            end_date=df["Date"].max()
+        ),
