638138450638b4cafb8a3dd177e1e6a8929769ad
natthapon3090 <garfield11kp@gmail.com>
2026-03-01 16:48:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,23 +11,3 @@ df = pd.read_csv("nvidia_stock.csv")
 df["Date"] = pd.to_datetime(df["Date"], errors="coerce")
 df = df.dropna()
 df = df.sort_values("Date")
-
-# ===============================
-# APP SETUP
-# ===============================
-
-app = Dash(__name__)
-
-])
-
-# ===============================
-# CALLBACK
-# ===============================
-
-@app.callback(
-    Output("price-chart", "figure"),
-    Output("volume-chart", "figure"),
-    Output("ma-chart", "figure"),
-    Input("date-picker", "start_date"),
-    Input("date-picker", "end_date"),
-    Input("ma-dropdown", "value")
