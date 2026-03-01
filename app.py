2ce24eec30ef084984168120bac4591016d94851
natthapon3090 <garfield11kp@gmail.com>
2026-03-01 16:46:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,37 +11,3 @@ df = pd.read_csv("nvidia_stock.csv")
 df["Date"] = pd.to_datetime(df["Date"], errors="coerce")
 df = df.dropna()
 df = df.sort_values("Date")
-
-# ===============================
-# APP SETUP
-# ===============================
-
-app = Dash(__name__)
-app.title = "NVIDIA Dashboard"
-
-app.layout = html.Div([
-
-    html.H1("NVIDIA Stock Dashboard", style={"textAlign": "center"}),
-
-    html.Div([
-        dcc.DatePickerRange(
-            id="date-picker",
-            start_date=df["Date"].min(),
-            end_date=df["Date"].max()
-        ),
-
-        dcc.Dropdown(
-            id="ma-dropdown",
-            options=[
-                {"label": "MA 20", "value": 20},
-                {"label": "MA 50", "value": 50}
-            ],
-            value=20
-        )
-    ], style={"width": "60%", "margin": "auto"}),
-
-    dcc.Graph(id="price-chart"),
-    dcc.Graph(id="volume-chart"),
-    dcc.Graph(id="ma-chart")
-
-])
