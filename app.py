7eb5876586617089a55a45286e7673b6fa127ab3
natthapon3090 <garfield11kp@gmail.com>
2026-03-01 16:48:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -17,32 +17,6 @@ df = df.sort_values("Date")
 # ===============================
 
 app = Dash(__name__)
-app.title = "NVIDIA Dashboard"
-
-app.layout = html.Div([
-
-    html.H1("NVIDIA Stock Dashboard", style={"textAlign": "center"}),
-
-    html.Div([
-        dcc.DatePickerRange(
-            id="date-picker",
-            start_date=df["Date"].min(),
-            end_date=df["Date"].max()
-        ),
-
-        dcc.Dropdown(
-            id="ma-dropdown",
-            options=[
-                {"label": "MA 20", "value": 20},
-                {"label": "MA 50", "value": 50}
-            ],
-            value=20
-        )
-    ], style={"width": "60%", "margin": "auto"}),
-
-    dcc.Graph(id="price-chart"),
-    dcc.Graph(id="volume-chart"),
-    dcc.Graph(id="ma-chart")
 
 ])
 
